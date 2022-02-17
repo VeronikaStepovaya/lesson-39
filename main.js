d3f5dae9e47828fceabda946399c04ef2f7b402a
VeronikaStepovaya <92208119+VeronikaStepovaya@users.noreply.github.com>
2022-02-17 20:59:59 +0200

    lesson39

---
+++ main.js
@@ -0,0 +1,44 @@
+   //8.1
+function display_message(){
+    alert("Единороги существуют")
+}
+display_message()
+
+    //8.2
+// let title = "Harry Potter"
+// function favorite_book(title){
+//     alert(`One of my favorite book is ${title}.`)
+// }
+// favorite_book(title)
+
+    //8.3
+// let raz = prompt("Введите размер футболки (S,M,L,XL): ")
+// let text =prompt("Введите текст футболки: ")
+// function make_shirt(raz, text){
+//     alert(`Вы заказали футболку размера ${raz} и с текстом на ней "${text}".`)
+// }
+// make_shirt(raz, text)
+
+    //8.4
+// function make_shirt(a, b){
+//     let raz = "L"
+//     let text = "I love Java Script"
+//     alert(`Вы выбрали футболку с размером ${raz} и текстом "${text}"`)
+// }
+// let a = prompt("Введите размер футболки (S,M,L,XL): ")
+// let b =prompt("Введите текст футболки: ")
+// make_shirt(a, b)
+// alert(`Вы выбрали футболку размера ${a} и с текстом на ней "${b}".`)
+
+
+    //8.5
+// let c = prompt("Введите название города")
+// function describe_city(a){
+//     let count = "Украина"
+//     alert(${c} это ${count})
+// }
+// let a = prompt("Введите название страны")
+// alert(${c} это ${a})
+// describe_city(a)
+// describe_city(a)
+// describe_city(a)
